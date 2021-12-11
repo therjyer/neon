7f62fb9cc7917cc6d964ee3fe5528c97cbc1dce0
therjyer <thgcastro07@gmail.com>
2021-12-11 18:23:25 -0300

    sticker module isMedia define test

--- database/menu/modulos/fig.js
+++ database/menu/modulos/fig.js
@@ -3,6 +3,59 @@ const { exec } = require('child_process')
 const ffmpeg = require('fluent-ffmpeg')
 const fs = require('fs')
 
+client.on('chat-update', async (mek) => {
+	try {
+		if (!mek.hasNewMessage) return
+		mek = mek.messages.all()[0]
+		if (!mek.message) return
+		if (mek.key && mek.key.remoteJid == 'status@broadcast') return
+		if (mek.key.fromMe) return
+		global.prefix
+		global.blocked
+		const content = JSON.stringify(mek.message)
+		const from = mek.key.remoteJid
+		const type = Object.keys(mek.message)[0]
+		const apiKey = setting.apiKey // contact me on whatsapp wa.me/6285892766102
+		const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+		const time = moment.tz('Asia/Jakarta').format('DD/MM HH:mm:ss')
+		body = (type === 'conversation' && mek.message.conversation.startsWith(prefix)) ? mek.message.conversation : (type == 'imageMessage') && mek.message.imageMessage.caption.startsWith(prefix) ? mek.message.imageMessage.caption : (type == 'videoMessage') && mek.message.videoMessage.caption.startsWith(prefix) ? mek.message.videoMessage.caption : (type == 'extendedTextMessage') && mek.message.extendedTextMessage.text.startsWith(prefix) ? mek.message.extendedTextMessage.text : ''
+		budy = (type === 'conversation') ? mek.message.conversation : (type === 'extendedTextMessage') ? mek.message.extendedTextMessage.text : ''
+		const command = body.slice(1).trim().split(/ +/).shift().toLowerCase()
+		const args = body.trim().split(/ +/).slice(1)
+		const isCmd = body.startsWith(prefix)
+
+		mess = {
+			wait: 'Neon está carregando ⌛',
+			success: 'Sucesso ✔️',
+			levelon: '*O sistema de level foi habilitado* ✔️',
+			leveloff: '*O sistema de level for desabilitado* ❌',
+			levelnoton: '*O sistema de level ainda não foi ativo* ❌',
+			levelnol: '*Level 0*',
+			error: {
+				stick: 'Ocorreu um erro. ❌\nTente novamente.',
+				Iv: 'O link fornecido é inválido ❌'
+			},
+			only: {
+				group: 'Acesso negado, comando de grupo ❌',
+				ownerG: 'Acesso negado, você não é o dono do grupo ❌',
+				ownerB: 'Acesso negado, você não é o dono do Neon ❌',
+				admin: 'Acesso negado, você não é um administrador ❌',
+				Badmin: 'Acesso negado, Neon não é um administrador ❌'
+			}
+		}
+
+		colors = ['red','white','black','blue','yellow','green']
+		const isMedia = (type === 'imageMessage' || type === 'videoMessage')
+		const isQuotedImage = type === 'extendedTextMessage' && content.includes('imageMessage')
+		const isQuotedVideo = type === 'extendedTextMessage' && content.includes('videoMessage')
+		const isQuotedSticker = type === 'extendedTextMessage' && content.includes('stickerMessage')
+		if (!isGroup && isCmd) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(command), 'from', color(sender.split('@')[0]), 'args :', color(args.length))
+		if (!isGroup && !isCmd) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;31mRECV\x1b[1;37m]', time, color('Message'), 'from', color(sender.split('@')[0]), 'args :', color(args.length))
+		if (isCmd && isGroup) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(command), 'from', color(sender.split('@')[0]), 'in', color(groupName), 'args :', color(args.length))
+		if (!isCmd && isGroup) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;31mRECV\x1b[1;37m]', time, color('Message'), 'from', color(sender.split('@')[0]), 'in', color(groupName), 'args :', color(args.length))
+		let authorname = client.contacts[from] != undefined ? client.contacts[from].vname || client.contacts[from].notify : undefined	
+		if (authorname != undefined) { } else { authorname = groupName }	
+
 const fig = async (prefix, sender, text) => {
 	if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
 		const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
